4a55f224eb7c31de4335d5a9ad9c476f77a15de0
marwane1978 <msouaf@msn.com>
2025-11-27 00:13:09 +0000

    Implement routing with React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
 
 export default function App() {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
 }
