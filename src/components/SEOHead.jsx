c51f535058f68147d08d92e77d561e9a73062256
marwane1978 <msouaf@msn.com>
2025-11-27 02:49:16 +0000

    Update SEOHead.jsx

--- src/components/SEOHead.jsx
+++ src/components/SEOHead.jsx
@@ -1,19 +1,21 @@
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 
 export default function SEOHead({ title, description, keywords, url, image }) {
   return (
     <Helmet>
       <title>{title}</title>
-      <meta name="description" content={description} />
 
+      <meta name="description" content={description} />
       {keywords && <meta name="keywords" content={keywords} />}
 
+      {/* Open Graph (Facebook, LinkedIn, etc.) */}
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       {url && <meta property="og:url" content={url} />}
       {image && <meta property="og:image" content={image} />}
       <meta property="og:type" content="website" />
 
+      {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
