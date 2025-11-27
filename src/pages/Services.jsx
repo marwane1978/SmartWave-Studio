c37e05ef2eb9ca800f4eb9583e03677ded55e012
marwane1978 <msouaf@msn.com>
2025-11-27 03:27:28 +0000

    Update Services.jsx

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -8,7 +8,9 @@ export default function Services() {
 
       <main className="px-10 py-10">
         <h1 className="text-4xl font-bold mb-4">Nos Services</h1>
-        <p className="text-gray-700">Page des services SmartWave Studio.</p>
+        <p className="text-gray-700">
+          Page des services SmartWave Studio.
+        </p>
       </main>
 
       <Footer />
