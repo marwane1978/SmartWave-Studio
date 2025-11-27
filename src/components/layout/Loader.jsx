f137b741ed5f9e74424541ba409bfe4bcafe44df
marwane1978 <msouaf@msn.com>
2025-11-27 01:58:36 +0000

    Update Loader.jsx

--- src/components/layout/Loader.jsx
+++ src/components/layout/Loader.jsx
@@ -1,20 +1,22 @@
+import React from "react";
+
 export default function Loader() {
   return (
-    <div className="loader-container">
-      <div className="loader-content">
+    <div className="fixed inset-0 bg-white flex items-center justify-center z-50 opacity-0 animate-fadeInOut">
+      <div className="flex flex-col items-center justify-center">
 
-        {/* LOGO */}
+        {/* Cercle animé */}
+        <div className="relative w-24 h-24 mb-6">
+          <div className="absolute inset-0 border-4 border-blue-200 rounded-full"></div>
+          <div className="absolute inset-0 border-4 border-blue-500 border-t-transparent rounded-full animate-spin-slow"></div>
+        </div>
+
+        {/* Logo */}
         <img
           src="/logo.png"
-          alt="SmartWave Logo"
-          className="loader-logo"
+          alt="SmartWave"
+          className="w-40 opacity-90 animate-pulse"
         />
-
-        {/* CERCLE 1 */}
-        <span className="circle circle1"></span>
-
-        {/* CERCLE 2 */}
-        <span className="circle circle2"></span>
       </div>
     </div>
   );
