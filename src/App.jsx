432d3f8b8c3a7015241ae80b23692bec97c87dca
marwane1978 <msouaf@msn.com>
2025-11-27 03:35:18 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 
 import Home from "./pages/Home";
