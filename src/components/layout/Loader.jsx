424f31971c909864efbbb437fec2c17b88f967dd
marwane1978 <msouaf@msn.com>
2025-11-27 01:53:58 +0000

    Update Loader.jsx

--- src/components/layout/Loader.jsx
+++ src/components/layout/Loader.jsx
@@ -1,15 +1,16 @@
 export default function Loader() {
   return (
-    <div className="flex items-center justify-center h-screen bg-[#0B1120]">
+    <div className="flex items-center justify-center h-screen bg-gray-900">
       <div className="relative flex items-center justify-center">
-        {/* Cercle animé */}
-        <div className="w-40 h-40 border-4 border-blue-500/30 border-t-blue-400 rounded-full animate-spin"></div>
 
-        {/* Logo */}
+        {/* Cercle animé */}
+        <div className="w-40 h-40 border-4 border-blue-500/20 border-t-blue-400 rounded-full animate-spin-slow"></div>
+
+        {/* Logo au centre */}
         <img
           src="/logo.png"
           alt="SmartWave Logo"
-          className="absolute w-24 h-24 object-contain drop-shadow-lg"
+          className="absolute w-24 drop-shadow-lg"
         />
       </div>
     </div>
